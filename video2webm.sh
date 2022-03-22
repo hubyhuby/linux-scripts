312591dca5f9f0dd8d16a428f73b56d7f4ba8957
hubyhuby <chomisyann@yahoo.fr>
2022-03-22 14:55:18 +0100

    Select file in GUI with zenity

--- video2webm.sh
+++ video2webm.sh
@@ -1,1 +1,9 @@
-ffmpeg -i --------------.mp4 -vcodec libvpx -acodec libvorbis ----------.webm
+
+#! /bin/bash
+echo -e "\e[32mThis script convert the files to WEBM using ffmpeg.\n \e[0m"
+
+folder="$(zenity  --file-selection --title="Choose a MP4 file " --file-selection)"
+echo "Converting the video, please wait..."
+notify-send "Converting the video, please wait..."
+echo "$folder"
+ffmpeg -i $folder -vcodec libvpx -acodec libvorbis $folder.webm

ac0acea29d817c7cddc8f313bb1796f17e389766
hubyhuby <chomisyann@yahoo.fr>
2022-03-23 09:01:07 +0100

    Optimize for speed : 720P@30FPS + "best" quality

--- video2webm.sh
+++ video2webm.sh
@@ -1,10 +1,10 @@
 
 #! /bin/bash
-echo -e "\e[32mThis script convert the files to WEBM using ffmpeg.\n \e[0m"
+echo -e "\e[32mThis script convert videos to 720P@30FPS WEBM using ffmpeg in an acceptable time on 2020 CPUs(~2 times slower thzn zctual videos).\n Using -deadline good , you may try -deadline realtime. \e[0m"
 
-folder="$(zenity  --file-selection --title="Choose a MP4 file " --file-selection)"
-echo "Converting the video, please wait..."
-notify-send "Converting the video, please wait..."
+folder="$(zenity  --file-selection --title="Choose a video file " --file-selection)"
+echo "Converting the video, please wait (encoding will take ~same time as the duration of the video) ..."
+notify-send "Converting the video, please wait for quite some time ..."
 echo "$folder"
-ffmpeg -i $folder -vcodec libvpx -deadline realtime -acodec libvorbis $folder.webm
+ffmpeg -i $folder -vf scale=-1:720,fps=30  -vcodec libvpx -deadline good -acodec libvorbis $folder.webm
 #ffmpeg -i _____.mp4  -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis ____.webm

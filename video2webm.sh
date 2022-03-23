d5f20ab0c96d2a603a9bbccdf2f10b9db723cdd5
hubyhuby <chomisyann@yahoo.fr>
2022-03-23 07:49:48 +0100

    -deadline realtime : encodes the file at the same speed it reads it

--- video2webm.sh
+++ video2webm.sh
@@ -6,4 +6,5 @@ folder="$(zenity  --file-selection --title="Choose a MP4 file " --file-selection
 echo "Converting the video, please wait..."
 notify-send "Converting the video, please wait..."
 echo "$folder"
-ffmpeg -i $folder -vcodec libvpx -acodec libvorbis $folder.webm
+ffmpeg -i $folder -vcodec libvpx -deadline realtime -acodec libvorbis $folder.webm
+#ffmpeg -i _____.mp4  -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis ____.webm

2a5b54477bfe0c499296c4c8c65fc7596987df1d
hubyhuby <chomisyann@yahoo.fr>
2023-01-04 16:37:19 +0100

    grive2 : pause

--- grive2_compile_install.sh
+++ grive2_compile_install.sh
@@ -2,6 +2,7 @@
 #! /bin/bash
 echo -e "\e[32m UNTESTED : should compile and install Grive 2\e[0m"
 #https://github.com/vitalif/grive2/issues/374
+read -rsp $'Press any key to continue...\n' -n1 key
 
 echo"Download dependency before compiling"
 sudo apt-get install git cmake build-essential libgcrypt20-dev libyajl-dev \

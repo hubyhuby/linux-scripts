4fc5e9bd71d405459836f685192b309616d3f23d
hubyhuby <chomisyann@yahoo.fr>
2023-01-04 16:33:23 +0100

    UNTESTED : should compile and install Grive 2

---
+++ grive2_compile_install.sh
@@ -0,0 +1,18 @@
+
+#! /bin/bash
+echo -e "\e[32m UNTESTED : should compile and install Grive 2\e[0m"
+#https://github.com/vitalif/grive2/issues/374
+
+echo"Download dependency before compiling"
+sudo apt-get install git cmake build-essential libgcrypt20-dev libyajl-dev \
+    libboost-all-dev libcurl4-openssl-dev libexpat1-dev libcppunit-dev binutils-dev \
+    debhelper zlib1g-dev dpkg-dev pkg-config
+
+echo " download project + compile + install"
+
+git clone https://github.com/vitalif/grive2.git
+cd grive2
+mkdir build
+cd build
+cmake ..
+sudo make -j4 install
